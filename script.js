adfa9e7a9df658d0c99a9bf253cc81693b5eb5e8
kessius00 <kes.olling00@gmail.com>
2022-09-07 01:45:07 +0200

    Making operators work with keypresses

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const nums = document.querySelectorAll('.num');
 const operators = document.querySelectorAll('.operator');
 const equalSign = document.querySelector('.equal');
 const allClear = document.querySelector('.allClear');
-const deleteChar = document.querySelector('.deleteChar');
+const deleteCharButton = document.querySelector('.deleteChar');
 const dot = document.querySelector('.dot');
 
 const currentValueDisplay = document.querySelector('.currentValue');
@@ -14,6 +14,7 @@ let firstNum = '';
 let secondNum = '';
 let currentOperation = null;
 let lastButtonPressed = null;
+let operatorChosen;
 
 
 
@@ -22,7 +23,70 @@ let lastButtonPressed = null;
 window.addEventListener('keydown', handleKeyboardInput);
 equalSign.addEventListener('click', evaluate);
 allClear.addEventListener('click', clear);
-deleteChar.addEventListener('click', ()=>{
+deleteCharButton.addEventListener('click', deleteChar);
+
+dot.addEventListener('click', addDot);
+
+operators.forEach((operator)=>{
+    operator.addEventListener('click', ()=>{
+        operatorChoice(operator.textContent);
+        doOperation(operator.textContent);
+    })
+});
+
+nums.forEach((num)=>{
+    num.addEventListener('click', ()=>{
+        if (currentValueDisplay.textContent.length<13){
+            appendNumber(num.textContent);
+        } else {
+            clear();
+            currentValueDisplay.textContent = 'Number too long!';
+        };
+        lastButtonPressed = num.textContent;
+    });
+});
+
+//Functions
+function doOperation(operatorChosen){
+    if (currentOperation !== null && secondNum !== ''){ //for continuing the calculation without equalSign
+        firstNum = operate(currentOperation, Number(firstNum), Number(secondNum)).toString(); 
+        currentValueDisplay.textContent = firstNum;
+        secondNum = '';
+    }
+    lastButtonPressed = operatorChosen;
+    if (operatorChosen == 'add') operatorChosen = '+';
+    if (operatorChosen == 'subtract') operatorChosen = '-';
+    if (operatorChosen == 'divide') operatorChosen = '÷';
+    if (operatorChosen == 'multiply') operatorChosen = '×';
+    backgroundValue.textContent = firstNum + ' ' + operatorChosen;
+}
+
+function evaluate(){
+    if (currentOperation === null) return 
+    if ( currentOperation === 'divide' && Number(secondNum) === 0){
+        currentValueDisplay.textContent = 'Dividing by zero: infinity!';
+        backgroundValue.textContent = '';
+    } else {
+        let answer = round(operate(currentOperation, Number(firstNum), Number(secondNum))).toString();
+        clear();
+        currentValueDisplay.textContent = answer;
+        firstNum = answer;
+        lastButtonPressed = '=';
+    }
+}
+
+function addDot(){
+    if (currentValueDisplay.textContent.includes('.')) return
+    appendNumber('.');
+    lastButtonPressed = '.';
+}
+
+function round(num){
+    num *= 10**9;
+    return Math.round(num)/(10**9)
+}
+
+function deleteChar(){
     if (lastButtonPressed === '='){
         clear();
     } else {
@@ -39,73 +103,22 @@ deleteChar.addEventListener('click', ()=>{
             secondNum = currentValueDisplay.textContent;
         }
     }
-});
-
-dot.addEventListener('click', ()=>{
-    if (currentValueDisplay.textContent.includes('.')) return 
-    appendNumber('.');
-    lastButtonPressed = '.';
-});
-
-operators.forEach((operator)=>{
-    operator.addEventListener('click', ()=>{
-        if (currentOperation !== null && secondNum !== ''){ //for continuing the calculation without equalSign
-            firstNum = operate(currentOperation, Number(firstNum), Number(secondNum)).toString(); 
-            currentValueDisplay.textContent = firstNum;
-            secondNum = '';
-        }
-        operatorChoice(operator.textContent);
-        lastButtonPressed = operator.textContent;
-        backgroundValue.textContent = firstNum + ' ' + operator.textContent;
-    })
-});
-
-nums.forEach((num)=>{
-    num.addEventListener('click', ()=>{
-        if (lastButtonPressed === '='){
-            clear();
-        } 
-        if (currentValueDisplay.textContent.length<13){
-            appendNumber(num.textContent);
-        } else {
-            clear();
-            currentValueDisplay.textContent = 'Number too long!';
-        };
-        lastButtonPressed = num.textContent;
-    });
-});
-
-//Functions
-function evaluate(){
-    if (currentOperation === null) return 
-    let answer = round(operate(currentOperation, Number(firstNum), Number(secondNum))).toString();
-    clear();
-    currentValueDisplay.textContent = answer;
-    firstNum = answer;
-    lastButtonPressed = '=';
 }
 
-function round(num){
-    num *= 10**9;
-    return Math.round(num)/(10**9)
-}
-
-function getCurrentNum(){
-    if (secondNum === '' && currentOperation === null){
-        return firstNum
-    } 
-    return secondNum
-}
 
 function clear(){
     firstNum = '';
     secondNum = '';
     currentOperation = null;
     backgroundValue.textContent = '';
-    currentValueDisplay.textContent = firstNum;
+    currentValueDisplay.textContent = '';
+    lastButtonPressed = 'cl';
 }
 
 function appendNumber(num){
+    if (lastButtonPressed === '='){
+        clear();
+    } 
     if (secondNum === '' && currentOperation === null){
         firstNum += num;
         currentValueDisplay.textContent = firstNum;
@@ -116,43 +129,18 @@ function appendNumber(num){
 }
 
 function handleKeyboardInput(e){
-    
     if (e.key >= 0 && e.key <=9) appendNumber(e.key);
-
-    switch (e.key){
-        case 'Enter':
-            console.log('enter');
-            break;
-        case 'Backspace':
-            console.log('backspace');
-            break;
-        case 'Escape':
-            console.log('escape');
-            break;
-        case '/':
-            currentOperation = 'divide'
-            console.log(currentOperation);
-
-            break;
-        case '*':
-            currentOperation = 'multiply'
-            console.log(currentOperation);
-
-
-            break;
-        case '+':
-            currentOperation = 'add'
-            console.log(currentOperation);
-
-            break;
-        case '-':
-            currentOperation = 'subtract'
-            console.log(currentOperation);
-
-            break; 
-        
-    }
+    if (e.key === 'Enter') evaluate();
+    if (e.key === 'Escape') clear();
+    if (e.key === 'Backspace') deleteChar();
+    if (e.key === '.') addDot();
+    if (e.key === ',') addDot();
+    if (e.key === '/') {doOperation('divide'); currentOperation = 'divide';}
+    if (e.key === '*') {doOperation('multiply'); currentOperation = 'multiply';}
+    if (e.key === '+') {doOperation('add'); currentOperation = 'add';}
+    if (e.key === '-') {doOperation('subtract'); currentOperation = 'subtract';}
 }
+
 
 function operate(operator, num1, num2){
     let answer;
